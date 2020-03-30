2ea1c616c8dc87734df3ef8b88ab75a0046d8524
emmeiwhite <emmeiwhite@gmail.com>
2020-03-30 15:55:12 +0530

    Now we are also showing the current gradient properties

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
+var css = document.querySelector(".css");
 var mainWrapper = document.querySelector(".background-wrapper");
 
 function setBackgroundGradient(event) {
   mainWrapper.style.backgroundImage =
     "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+  css.textContent = mainWrapper.style.backgroundImage + ";";
 }
 
 color1.addEventListener("input", setBackgroundGradient);
